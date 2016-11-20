4211c9ad24be72d4061f50cc1d3874deef867ad2
Elif Sezgin <elifmsezgin@gmail.com>
2016-11-20 15:18:39 -0800

    Add render method to board class

--- board.rb
+++ board.rb
@@ -1,8 +1,24 @@
-class Board
+require_relative "tile"
 
-  def initialize(grid = Array.new(9) {Array.new(9) {Tile.new}, bomb_count = 10)
-    @grid = grid
+class Board
+  attr_reader :grid
+  def initialize(bomb_count = 10)
+    @grid = (Array.new(9) { Array.new(9) { Tile.new } })
     @bomb_count = bomb_count
+  end
+
+  def set_pos
+    grid.each_with_index do |row, idx1|
+      row.each_with_index do |cell, idx2|
+        cell.set_pos([idx1, idx2])
+      end
+    end
+  end
+
+  def run
+    set_pos
+    plant_bombs(@bomb_count)
+    render
   end
 
   def plant_bombs(bomb_count)
@@ -10,10 +26,11 @@ class Board
     until counter == bomb_count
       pos = [rand(9), rand(9)]
       if valid_plant?(pos)
-        grid[pos].bomb
+        self[pos].bomb
         counter += 1
       end
     end
+
   end
 
   def [](pos)
@@ -27,12 +44,62 @@ class Board
   end
 
   def render
+    display = []
+    grid.each do |row|
 
-
+      row.each do |cell|
+        if cell.flagged
+          display << "[f] "
+        else
+          if cell.revealed
+            display << (cell.bombed ? "[X] " : "[#{cell.neighbor_bomb_count(self)}] ")
+          else
+            display << "[ ] "
+          end
+        end
+      end
+      display << "\n"
+    end
+    puts display.join("")
   end
 
+  def neighbors(pos)
+    row, col = pos
+    all_neighbor_positions = []
+    all_directions = {
+      left: [row, col - 1],
+      right: [row, col + 1],
+      up: [row - 1, col],
+      down: [row + 1, col],
+      up_left: [row - 1, col - 1],
+      up_right: [row - 1, col + 1],
+      down_left: [row + 1, col - 1],
+      down_right: [row + 1, col + 1]
+    }
+    all_directions.values.each do |pos|
+      if valid_pos?(pos)
+        all_neighbor_positions << pos
+      end
+    end
+    all_neighbor_positions
+  end
+
+  def valid_pos?(pos)
+    begin
+      if self[pos]
+        true
+      end
+    rescue
+      false
+    end
+  end
+
+
   def valid_plant?(pos)
-    grid[pos].bombed ? false : true
+    self[pos].bombed ? false : true
   end
 
 end
+
+board = Board.new
+board.run
--- tile.rb
+++ tile.rb
@@ -1,14 +1,29 @@
+require_relative "board"
+
 class Tile
 
-  attr_reader :bombed, :revealed, :flagged
+  attr_reader :bombed, :revealed, :flagged, :pos
   # attr_accessor :pos
 
-  def initialize(board)
+  def initialize
     @bombed = false
     @revealed = false
     @flagged  = false
-    @board = board
-    # @pos = nil
+    # @board = board
+    @pos = nil
+  end
+  #
+  # def self.set_board(board)
+  #   @board = board
+  #   board.grid.each_with_index do |row, idx1|
+  #     row.each_with_index do |cell, idx2|
+  #       cell.set_pos([idx1, idx2])
+  #     end
+  #   end
+  # end
+
+  def set_pos(pos)
+    @pos = pos
   end
 
   def reveal
@@ -16,9 +31,26 @@ class Tile
   end
 
   def neighbors
+    all_neighbors = []
+    neighbor_positions = @board.neighbors(self.pos)
+    neighbor_positions.each do |pos|
+      all_neighbors << board[pos]
+    end
+    all_neighbors
   end
 
-  def neighbor_bomb_count
+  def neighbor_bomb_count(board)
+    @board = board
+
+    count = 0
+    # neighbors = neighbors(@board)
+
+    neighbors.each do |tile|
+      if tile.bombed
+        count += 1
+      end
+    end
+    count
   end
 
   def bomb
